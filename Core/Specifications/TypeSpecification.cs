5073a5bfe7d182bd9ed94b11b3206e8d9df4ae47
Yasir Auleear <yasir.auleear@sdworx.com>
2026-01-06 09:47:22 +0400

    feat: add specification with projection

---
+++ Core/Specifications/TypeSpecification.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities;
+
+namespace Core.Specifications
+{
+    public class TypeSpecification : SpecificationsBase<Product, string>
+    {
+        public TypeSpecification()
+        {
+            AddSelect(x => x.Type);
+            ApplyDistinct();
+        }
+    }
+}
